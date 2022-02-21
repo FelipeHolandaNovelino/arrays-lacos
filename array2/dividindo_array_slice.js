214d24108e79effc085c0719d71417a4a016818c
Felipe Novelino <91998237+FelipeHolandaNovelino@users.noreply.github.com>
2022-02-21 15:10:35 -0300

    .slice()

--- array2/dividindo_array_slice.js
+++ array2/dividindo_array_slice.js
@@ -4,7 +4,7 @@ const nomes = ['joao', 'juliana','Ana', 'Caio', 'Lara', 'Marjorie', "Guilherme",
                'Isabela', 'Vinicius', 'Renan', 'Renata', 'Daisy', 'Camilo'];
                         //declaramos indice 0 e nome.length 10,ou seja comeca no 0 e ira ate o 9
 const sala1 = nomes.slice(0,nomes.length/2);
-                        //aqui declaramos somente 10,ira comecar de 10 pra frente
+                        //aqui declaramos somente 10,ira comecar de 10 pra frente.
 const sala2 = nomes.slice(nomes.length/2);
 
 console.log(`Alunos da Sala 1: ${sala1}`)
