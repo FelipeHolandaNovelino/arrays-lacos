f9696a48e67e2fa5ce41d822fd6844fa5bb6f4a7
Felipe Novelino <91998237+FelipeHolandaNovelino@users.noreply.github.com>
2022-02-21 15:05:34 -0300

    push()

--- Array1/adicionando_nota.js
+++ Array1/adicionando_nota.js
@@ -1,6 +1,6 @@
                 //lista
 const notas = [10, 6, 8,];
-notas.push(7); // .push adiciona o valor () como um novo elemento a nossa lista
+notas.push(7); // .push adiciona o valor () como um novo elemento a nossa lista.
 console.log(notas)
 
 let media = (notas[0] + notas[1] + notas[2] + notas[3]) / notas.length;
