b1c90ea91abf07816ed815bc5e98e8b800422dec
Felipe Novelino <91998237+FelipeHolandaNovelino@users.noreply.github.com>
2022-02-21 15:10:08 -0300

    .concat()

--- array2/concat_arrays.js
+++ array2/concat_arrays.js
@@ -4,4 +4,4 @@ const salaDePython = ['Melisa','Helena','Rodrigo'];
 
 const salasUnificadas = salaDePython.concat(salaDeJavaScript);
 
-console.log(salasUnificadas)  //.concat nao modifica as arrays,por isso ele precisa ser o resultado de uma nova variavel
+console.log(salasUnificadas)  //.concat nao modifica as arrays,por isso ele precisa ser o resultado de uma nova variavel.
