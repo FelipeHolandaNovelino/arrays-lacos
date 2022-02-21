3f67806837877362f374516aa35f948e500bf370
Felipe Novelino <91998237+FelipeHolandaNovelino@users.noreply.github.com>
2022-02-21 15:08:19 -0300

    pop()

--- Array1/removendo_nota.js
+++ Array1/removendo_nota.js
@@ -1,5 +1,5 @@
 let notas = [10, 7, 8, 5, 10];
-notas.push() //.push nao aceita parametro no () e sempre ira remover o ultimo elemento da lista
+notas.pop() //.pop nao aceita parametro no () e sempre ira remover o ultimo elemento da lista
 console.log(notas)
 
 let media = (notas[0] + nota[1] + notas[2] + notas[3]) / notas.length
